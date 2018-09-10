b1a9925e726846f4c50fc5b0e403ff150452a508
qingyang <1234567victor@163.com>
2018-09-10 14:17:50 -0700

    own validation

--- src/com/luv2code/springdemo/mvc/Customer.java
+++ src/com/luv2code/springdemo/mvc/Customer.java
@@ -6,6 +6,8 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
+import com.luv2code.springdemo.mvc.validation.CourseCode;
+
 
 
 public class Customer {
@@ -16,12 +18,6 @@ public class Customer {
 	@Size(min=1,message="is required")
 	private String lastName;
 	
-	
-//	@NotNull(message="is required")
-//	@Min(value=0, message="must be greater than or equal to zero")
-//	@Max(value=10, message="must be less than or equal to 10")
-//	private Integer freePasses;
-	
 	@NotNull(message="is required")     
 	@Min(value=0, message="must be greater than or equal to zero")     
 	@Max(value=10, message="must be less than or equal to 10")     
@@ -30,8 +26,18 @@ public class Customer {
 	@Pattern(regexp="^[a-zA-Z0-9]{5}",message="only 5 chars/digits")
 	private String postalCode;
 	
+	@CourseCode
+	private String courseCode;
 	
 	
+	public String getCourseCode() {
+		return courseCode;
+	}
+
+	public void setCourseCode(String courseCode) {
+		this.courseCode = courseCode;
+	}
+
 	public Integer getFreePasses() {
 		return freePasses;
 	}
